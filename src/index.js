173f0fd05476ff4ed03194aefa5bbaa8dd912c19
Bekzat <esimhanuli13@gmail.com>
2023-06-06 11:16:16 +0600

    webpack config

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// fetch('https://api.weatherapi.com/v1/current.json?key=a045b3f481734899a3183450232405&q=london', {mode: 'cors'})
+//     .then(function(response) {
+//       console.log(response.json());
+//     });
